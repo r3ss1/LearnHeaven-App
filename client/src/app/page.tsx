2cc968ebc4f98cfd174bb7742ca4ece8c8946ace
aessary <raressilviulazar@gmail.com>
2025-01-09 22:16:45 +0000

    Landing page & Footer

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,16 @@
 import NonDashboardNavbar from "../components/NonDashboardNavbar";
+import Landing from "./(nondashboard)/Landing/Page";
+import Footer from "../components/Footer";
+
 
 export default function Home() {
   return (
     <div>
      <NonDashboardNavbar />
+     <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
+      <Footer />
     </div>
   );
 }
